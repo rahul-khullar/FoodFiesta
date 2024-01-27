fa806b497615b64daae670f46f9e3c103d4f422f
Rahul Khullar <khullarrahul09@gmail.com>
2024-01-27 16:39:58 +0530

    Intial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+const AppLayout = () => {
+    return(
+        <>
+        This is my Food App Food Fiesta !!!!!!!
+        </>
+    );
+};
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout /> );
